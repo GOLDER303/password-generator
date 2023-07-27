54c0de8e7bc4e1bed626beadbf0b774bdc9d7844
GOLDER303 <szymonstepien@interia.pl>
2023-07-27 21:10:14 +0200

    Add password generation logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,37 @@
-module Main where
+import Data.List
+import System.Random
+
+uppercaseLetters :: String
+uppercaseLetters = ['A' .. 'Z']
+
+lowercaseLetters :: String
+lowercaseLetters = ['a' .. 'z']
+
+digits :: String
+digits = ['0' .. '9']
+
+specialChars :: String
+specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?/`~"
+
+allChars :: String
+allChars = uppercaseLetters ++ lowercaseLetters ++ digits ++ specialChars
+
+randomIndex :: Int -> IO Int
+randomIndex n = randomRIO (0, n - 1)
+
+randomChar :: IO Char
+randomChar = do
+  index <- randomIndex (length allChars)
+  return (allChars !! index)
+
+generatePassword :: Int -> IO String
+generatePassword lengthOfPassword = do
+  mapM (const randomChar) [1 .. lengthOfPassword]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Enter the length of the password:"
+  input <- getLine
+  let lengthOfPassword = read input :: Int
+  password <- generatePassword lengthOfPassword
+  putStrLn $ "Generated password: " ++ password
